a9933bf42e93508d8f1d933d681ff28d15213144
nurulq <nqomariyah@polinela.ac.id>
2025-02-22 15:18:21 +0700

    complete Update & Delete features

--- todolist_project/todolist/views.py
+++ todolist_project/todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
@@ -13,8 +13,32 @@ def task_create(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('task_list')  # Arahkan kembali ke daftar tugas setelah sukses
+            return redirect('task_list')
     else:
         form = TaskForm()
 
     return render(request, 'todolist/task_create.html', {'form': form})
+
+
+def task_update(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'todolist/task_update.html', {'form': form, 'task': task})
+
+
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+
+    return render(request, 'todolist/task_delete.html', {'task': task})
