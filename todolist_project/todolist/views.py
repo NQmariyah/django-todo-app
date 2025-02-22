5c7699603f132a1bb2b5fb1cb76a61b3aa287d29
nurulq <nqomariyah@polinela.ac.id>
2025-02-22 15:01:48 +0700

    fix mistakes and bugs

--- todolist_project/todolist/views.py
+++ todolist_project/todolist/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todolist/task_list.html', {'tasks': tasks})
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')  # Arahkan kembali ke daftar tugas setelah sukses
+    else:
+        form = TaskForm()
+
+    return render(request, 'todolist/task_create.html', {'form': form})
